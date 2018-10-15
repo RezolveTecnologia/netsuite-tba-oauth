f618ef0e3bd33f6f1cce2da1e050055a0ef3411f
Jaime Ramos <jaime.ramos@rezolve.com.br>
2018-10-15 15:53:35 -0300

    TBA finished.

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
 # netsuite-tba-oauth
 NetSuite Token Based Authentication Module
+
+## Introduction:
+This auxiliary module abstracts the authentication mechanism used by NetSuite Restlets.
+
+## Supported authentication methods:
+Token Based Authentication (TBA)
+NLAuth not supported. Use the netsuite-restlet module if you need NLAuth authentication.
+
+## Supported Restlet methods:
+
+GET (get function)
+POST (post function)
+PUT (put function)
+
+## Installation
+
+Open a terminal session and enter the following command:
+
+``npm install netsuite-tba-auth --save``
+
+## Usage
+
+Example of GET request
+
+```javascript
+const NetSuiteOauth = require('netsuite-tba-oauth');
+
+const url = 'restlet-url';
+const method = 'GET';
+const consumerKey = 'your-consumer-key';
+const consumerSecret = 'your-consumer-secret';
+const tokenId = 'token-id';
+const tokenSecret = 'token-secret';
+const account = 'account Id';
+
+const oauth = new NetSuiteOauth(url, method, consumerKey, consumerSecret, tokenId, tokenSecret, account);
+
+oauth.get().then(response => console.log(response));
+```
+
+Example of POST request
+
+```javascript
+const NetSuiteOauth = require('netsuite-tba-oauth');
+
+const url = 'restlet-url';
+const method = 'GET';
+const consumerKey = 'your-consumer-key';
+const consumerSecret = 'your-consumer-secret';
+const tokenId = 'token-id';
+const tokenSecret = 'token-secret';
+const account = 'account Id';
+
+const oauth = new NetSuiteOauth(url, method, consumerKey, consumerSecret, tokenId, tokenSecret, account);
+const data = {key: 'value'};
+
+oauth.post(data).then(response => console.log(response));
+```
+
+
+
+
+
---
+++ app.js
@@ -0,0 +1,116 @@
+'use strict';
+
+// Dependencies
+const request = require('request');
+const OAuth   = require('oauth-1.0a');
+const crypto  = require('crypto');
+
+module.exports = NetSuiteOAuth;
+
+/**
+ * Constructor
+ *
+ * @param url
+ * @param method
+ * @param consumerKey
+ * @param consumerSecret
+ * @param tokenId
+ * @param tokenSecret
+ * @param account
+ * @returns {PromiseLike<ArrayBuffer>}
+ * @constructor
+ */
+function NetSuiteOAuth(url, method, consumerKey, consumerSecret, tokenId, tokenSecret, account) {
+    this.oauth = OAuth({
+        consumer: {
+            key: consumerKey,
+            secret: consumerSecret
+        },
+        realm: account,
+        signature_method: 'HMAC-SHA1',
+        hash_function(base_string, key) {
+            return crypto.createHmac('sha1', key).update(base_string).digest('base64');
+        }
+    });
+
+    this.request_data = {
+        url: url,
+        method: method
+    };
+
+    this.token = {
+        key: tokenId,
+        secret: tokenSecret
+    };
+
+    this.headers = this.oauth.toHeader(this.oauth.authorize(this.request_data, this.token));
+    this.headers['Content-Type'] = 'application/json';
+}
+
+NetSuiteOAuth.prototype.get = function() {
+    return new Promise((resolve, reject) => {
+        request({
+            url: this.request_data.url,
+            method: this.request_data.method,
+            headers: this.headers
+        }, function(error, response, body) {
+            // Process your data here
+            console.log(body);
+            if (error || response.statusCode.toString()[0] != 2) {
+                console.log('Body data:', body);
+                reject(body || error);
+            }
+            else {
+                if (typeof body == 'string') body = JSON.parse(body);
+                resolve(body || error);
+            }
+        });
+
+    });
+};
+
+NetSuiteOAuth.prototype.post = function(data) {
+    return new Promise((resolve, reject) => {
+        request({
+            url: this.request_data.url,
+            method: this.request_data.method,
+            json: data,
+            headers: this.headers
+        }, function(error, response, body) {
+            // Process your data here
+            console.log(body);
+            if (error || response.statusCode.toString()[0] != 2) {
+                console.log('Body data:', body);
+                reject(body || error);
+            }
+            else {
+                if (typeof body == 'string') body = JSON.parse(body);
+                resolve(body || error);
+            }
+        });
+
+    });
+};
+
+NetSuiteOAuth.prototype.put = function(data) {
+    return new Promise((resolve, reject) => {
+        request({
+            url: this.request_data.url,
+            method: this.request_data.method,
+            json: data,
+            headers: this.headers
+        }, function(error, response, body) {
+            // Process your data here
+            console.log(body);
+            if (error || response.statusCode.toString()[0] != 2) {
+                console.log('Body data:', body);
+                reject(body || error);
+            }
+            else {
+                if (typeof body == 'string') body = JSON.parse(body);
+                resolve(body || error);
+            }
+        });
+
+    });
+};
